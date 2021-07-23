9770ccce8e3a9d122362f5e5e3c69f21caafed76
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-07-23 15:53:11 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -22,11 +22,11 @@ class TEXT:
 
 ** Developer:** {bot_owner}
 
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+** Channel:** [KID BOTS](https://t.me/kidbots)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Group:** [KID SUPPORT](https://t.me/tzkid)
 
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** Source Code:** [Press Me](https://t.me/kid_of_telegram)
 """
 
     HELP_USER = """**Follow Below Steps:**
@@ -59,4 +59,4 @@ __--Payment Methods:--__
  
 GooglePay / Paytm / PhonPay / paypal / Net Banking
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @kid_of_telegram"""
